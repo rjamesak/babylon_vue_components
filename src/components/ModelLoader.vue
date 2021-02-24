9ca15a180d56369a4dc18a5ba5dc94b1b5fac51a
Russell James <jamesru@oregonstate.edu>
2021-02-23 23:05:38 -0900

    model loading setup to load from array of objects

--- src/components/ModelLoader.vue
+++ src/components/ModelLoader.vue
@@ -7,19 +7,43 @@
 import * as BABYLON from 'babylonjs';
     export default {
         name: 'ModelLoader', 
+        data() {
+            return {
+                sceneObjects:
+                    [
+                        {
+                            name: "",
+                            url: "http://localhost:8000/",
+                            filename: "Parrot.glb",
+                            position: {x: 0, y: 3, z: 0},
+                            scaling: 0.02,
+                        },
+                        {
+                            name: "",
+                            url: "http://localhost:8000/",
+                            filename: "Logo.gltf.glb",
+                            position: {x: -2, y: 2, z: -1},
+                            scaling: 0.1,
+                        }
+                    ]
+            }
+        },
         methods: {
             getModel() {
-                BABYLON.SceneLoader.ImportMeshAsync("", "http://localhost:8000/", "Parrot.glb").then((result) => {
-                        
-                    this.rootMesh = result.meshes[0];
-                    this.rootMesh.position = new BABYLON.Vector3(0, 3, 0);
-                    this.rootMesh.name = "parrot"
-                    this.rootMesh.scaling.scaleInPlace(0.02)
-                    //const birdAnim = scene.getAnimationGroupByName("parrot_A_")
-                    //console.log("birdAnim: ", birdAnim)
-                    this.$emit('model-created', this.rootMesh)
-                        
-                    }); // end model loading
+                this.sceneObjects.forEach(mesh => {
+                    BABYLON.SceneLoader.ImportMeshAsync(mesh.name, mesh.url, mesh.filename)
+                    .then((result) => {
+                        this.rootMesh = result.meshes[0];
+                        this.rootMesh.position = new BABYLON.Vector3(mesh.position.x, mesh.position.y, mesh.position.z);
+                        this.rootMesh.name = mesh.name
+                        this.rootMesh.scaling.scaleInPlace(mesh.scaling)
+                        //const birdAnim = scene.getAnimationGroupByName("parrot_A_")
+                        //console.log("birdAnim: ", birdAnim)
+                        this.$emit('model-created', this.rootMesh)
+                            
+                        }); // end model loading
+
+                })
 
 
 
@@ -37,6 +61,7 @@ import * as BABYLON from 'babylonjs';
             // fetch parameters
             console.log('modelLoader here...')
             this.getModel();
+            console.log("scene: ", this.scene);
             },
         }
 </script>
